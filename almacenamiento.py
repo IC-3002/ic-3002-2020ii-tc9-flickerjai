8b8ce59fe379993e4ba5a04947ebe3ab97c37e64
flickerjai <jaig1500@gmail.com>
2020-12-01 23:20:55 -0600

    Final

--- almacenamiento.py
+++ almacenamiento.py
@@ -13,37 +13,3 @@ def maximizar(As, D):
             return secuencia
 
         i = i + 1
-
-#------------------------------------------------------------------
-#                            Merge Sort
-#------------------------------------------------------------------
-def mergeSort(A): 
-    if len(A) >1: 
-        mid = len(A) // 2 
-        left = A[:mid]  
-        right = A[mid:] 
-  
-        mergeSort(left) 
-        mergeSort(right) 
-        i = j = k = 0
-          
-        while i < len(left) and j < len(right): 
-            if left[i][1] < right[j][1]: 
-                A[k] = left[i] 
-                i = i + 1
-            else: 
-                A[k] = right[j] 
-                j = j + 1
-            k+= 1
-          
-        while i < len(left): 
-            A[k] = left[i] 
-            i = i + 1
-            k = k + 1
-          
-        while j < len(right): 
-            A[k] = right[j] 
-            j = j + 1
-            k = k + 1
-
-    return A
